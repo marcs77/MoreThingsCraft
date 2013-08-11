dfca0a746e6338b950df1464949afc73b3b6c2f9
marcs77 <kinsisi360@hotmail.com>
2013-08-11 18:16:14 +0200

    Added item structure, cobalt ingot and es_ES.xml file.

--- mtc_common/com/marcs/mtc/lib/Reference.java
+++ mtc_common/com/marcs/mtc/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MOD_ID = "MTC";
 	public static final String MOD_NAME = "MoreThingsCraft";
-	public static final String VERSION = "0.0.7";
+	public static final String VERSION = "0.0.8";
 	public static final String CHANNEL_NAME = MOD_ID;
 	public static final String COMMON_PROXY_CLASS = "com.marcs.mtc.core.proxy.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "com.marcs.mtc.core.proxy.ClientProxy";
